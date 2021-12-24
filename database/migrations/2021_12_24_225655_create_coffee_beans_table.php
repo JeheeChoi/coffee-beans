1fa45a4c130e366a1985b718e26e34e99157c873
JeheeChoi <writetojehee@gmail.com>
2021-12-24 18:01:49 -0500

    Create model `CoffeeBean`
    and a table that holds coffee-beans data

---
+++ database/migrations/2021_12_24_225655_create_coffee_beans_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoffeeBeansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coffee_beans', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('coffee_beans');
+    }
+}
