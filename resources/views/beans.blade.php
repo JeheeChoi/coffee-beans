b20181ce7149634f7c9459be9a138b275e0d08af
JeheeChoi <writetojehee@gmail.com>
2021-12-25 01:13:18 -0500

    Add new route - get `beans` view page
    new beans.blade file and testing new get route - view page

---
+++ resources/views/beans.blade.php
@@ -0,0 +1,7 @@
+<h1>Beans List</h1>
+
+<ul>
+    @foreach ($beans as $beans)
+    <li>{{ $beans }}</li>
+    @endforeach
+</ul>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Simplified version of get page
+// First argument - url, second argument - view
+Route::view('/', 'welcome');
+
+// Get request for beans and return the view of beans
+Route::get('beans', function () {
+
+    $beans = [
+        'casi cielo',
+        'algo mas',
+        'cafe'
+    ];
+
+
+    return view('beans', [
+        'beans' => $beans,
+    ]);
 });
